5748a7632cf8ed239e87e6ad0eb3e510a9c4c1e4
“ibrahim-999” <ibrahimkhalaf99@gmail.com>
2023-07-15 19:55:15 +0300

    [Add] dependency injection for transaction service and create project controller

---
+++ app/Http/Controllers/Api/v1/ProjectController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Services\TransactionService;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    private TransactionService $transactionService;
+
+    public function __construct(TransactionService $transactionService)
+    {
+        $this->transactionService = $transactionService;
+    }
+
+    public function allProviders(Request $request)
+    {
+        $data = $this->transactionService->Providers();
+        if ($request->query('provider')) {
+            $data = $this->transactionService->showProvider($request->query('provider'));
+        }
+        if ($request->query('statusCode')) {
+            $data =  $this->transactionService->statusProvider($request->query('statusCode'), $data);
+        }
+        if ($request->query('currency')) {
+
+            $data = $this->transactionService->currency($request->query('currency'), $data);
+        }
+        if ($request->query('amounteMin') && $request->query('amounteMin')) {
+            $data =  $this->transactionService->amountRange($request, $data);
+        }
+        if ($data) {
+            return response()->json($data);
+        } else {
+            return response()->json('No data is available for this research');
+        }
+    }
+}
