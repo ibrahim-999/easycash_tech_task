dbb3dbd62863d019cc222755819ef369ab999ef9
“ibrahim-999” <ibrahimkhalaf99@gmail.com>
2023-07-15 19:31:29 +0300

    [Add] route providers

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'api/v1'], function () use ($router) {
+    $router->get('transactaions', 'Api\v1\ProjectController@allProviders');
+});
